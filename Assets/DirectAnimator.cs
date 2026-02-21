5c7578494edf4893d6e22f1395eeca2f182a2468
Santosh M <santosh09062004@users.noreply.github.com>
2026-02-21 23:36:07 +0530

    Initial commit

---
+++ Assets/DirectAnimator.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class DirectAnimator : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.Play("Run");
+        }
+    }
+}
